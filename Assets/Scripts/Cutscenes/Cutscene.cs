b00afa08ba4246cb37f7d51e8a1c9a84fec93b80
Mauro Delgaudio <mauro_delgaudio@outlook.com>
2021-02-24 19:39:48 -0300

    Test interactive intro cutscene

--- Assets/Scripts/Cutscenes/Cutscene.cs
+++ Assets/Scripts/Cutscenes/Cutscene.cs
@@ -1,10 +1,28 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 using UnityEngine.SceneManagement;
 
-public class Cutscene : MonoBehaviour
-{
+public class Cutscene : MonoBehaviour {
+    private PlayableDirector director;
+
+    private void Awake() {
+        director = GetComponent<PlayableDirector>();
+    }
+
+
+    private void Update() {
+        if (director.state == PlayState.Paused && Input.anyKeyDown) {
+            director.Play();
+        }
+    }
+
+    public void WaitForInput() {
+        director.Pause();
+    }
+
     public void LoadLevel(int index)
     {
         Scene selectedScene = SceneManager.GetSceneByBuildIndex(index);
